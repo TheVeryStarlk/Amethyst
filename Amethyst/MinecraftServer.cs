b330d9534d7a6f78b1404d2f798355a2b1629047
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-03-02 21:32:41 +0300

    Create project

---
+++ Amethyst/MinecraftServer.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using Microsoft.Extensions.Logging;
+
+namespace Amethyst;
+
+internal sealed class MinecraftServer(MinecraftServerConfiguration configuration) : IMinecraftServer
+{
+    private readonly ILogger<MinecraftServer> logger = configuration.LoggerFactory.CreateLogger<MinecraftServer>();
+    private readonly IPEndPoint endPoint = configuration.ListeningEndPoint;
+}
+
+internal interface IMinecraftServer;
---
+++ Amethyst/MinecraftServerConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using Microsoft.Extensions.Logging;
+
+namespace Amethyst;
+
+internal sealed class MinecraftServerConfiguration
+{
+    public required IPEndPoint ListeningEndPoint { get; init; }
+
+    public required ILoggerFactory LoggerFactory { get; init; }
+}
