9401b63ca02da07c94139b0dc63701b8a749d04b
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-03-02 23:04:24 +0300

    Replace init with set

--- Amethyst/MinecraftServerConfiguration.cs
+++ Amethyst/MinecraftServerConfiguration.cs
@@ -12,20 +12,20 @@ internal sealed class MinecraftServerConfiguration
     /// <summary>
     /// The remote <see cref="IPEndPoint"/> the <see cref="MinecraftServer"/> should listen to.
     /// </summary>
-    public required IPEndPoint ListeningEndPoint { get; init; }
+    public required IPEndPoint ListeningEndPoint { get; set; }
 
     /// <summary>
     /// Sets the message of the day of the <see cref="MinecraftServer"/> as known as MOTD.
     /// </summary>
-    public required Chat MessageOfTheDay { get; init; }
+    public required Chat MessageOfTheDay { get; set; }
 
     /// <summary>
     /// Sets the maximum amount of players that can join the <see cref="MinecraftServer"/>.
     /// </summary>
-    public required int MaximumPlayerCount { get; init; }
+    public required int MaximumPlayerCount { get; set; }
 
     /// <summary>
     /// The logger factory which <see cref="MinecraftServer"/> uses to create loggers.
     /// </summary>
-    public required ILoggerFactory LoggerFactory { get; init; }
+    public required ILoggerFactory LoggerFactory { get; set; }
 }
