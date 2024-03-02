a37584f9edb647a712a7beddb6d8763b556c7043
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-03-02 22:17:02 +0300

    Add shutdown

--- Amethyst/MinecraftServer.cs
+++ Amethyst/MinecraftServer.cs
@@ -7,9 +7,22 @@ internal sealed class MinecraftServer(MinecraftServerConfiguration configuration
 {
     private readonly ILogger<MinecraftServer> logger = configuration.LoggerFactory.CreateLogger<MinecraftServer>();
     private readonly IPEndPoint listeningEndPoint = configuration.ListeningEndPoint;
+    private readonly CancellationTokenSource source = new CancellationTokenSource();
+
+    public void Shutdown()
+    {
+        logger.LogDebug("Shutting down the server");
+        source.Cancel();
+    }
 }
 
 /// <summary>
 /// Represents a Minecraft server.
 /// </summary>
-internal interface IMinecraftServer;
+internal interface IMinecraftServer
+{
+    /// <summary>
+    /// Stops listening for connections & shutdowns the server's tasks.
+    /// </summary>
+    public void Shutdown();
+}
