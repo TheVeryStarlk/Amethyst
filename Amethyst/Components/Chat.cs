895d12a0a504761c28ec6537ef40ea4ef92dd76f
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-03-02 22:25:58 +0300

    Add different Minecraft components

---
+++ Amethyst/Components/Chat.cs
@@ -0,0 +1,41 @@
+﻿namespace Amethyst.Components;
+
+internal sealed class Chat
+{
+    public string Text { get; set; } = string.Empty;
+
+    public bool Bold { get; set; }
+
+    public bool Italic { get; set; }
+
+    public bool Underlined { get; set; }
+
+    public bool StrikeThrough { get; set; }
+
+    public bool Obfuscated { get; set; }
+
+    public string Color { get; set; } = "white";
+
+    public Chat[]? Extra { get; set; }
+
+    public static Chat Create(
+        string text,
+        bool bold = false,
+        bool italic = false,
+        bool underlined = false,
+        bool strikeThrough = false,
+        bool obfuscated = false,
+        string color = "white")
+    {
+        return new Chat
+        {
+            Text = text,
+            Bold = bold,
+            Italic = italic,
+            Underlined = underlined,
+            StrikeThrough = strikeThrough,
+            Obfuscated = obfuscated,
+            Color = color
+        };
+    }
+}
---
+++ Amethyst/Components/Status.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Amethyst.Components;
+
+internal sealed class Status
+{
+    public required ServerVersion Version { get; set; }
+
+    [JsonPropertyName("players")]
+    public required PlayerInformation PlayerInformation { get; set; }
+
+    public required Chat Description { get; set; }
+
+    public string Favicon { get; set; } = string.Empty;
+
+    public static Status Create(
+        string name,
+        int protocol,
+        int max,
+        int online,
+        Chat description,
+        string favicon = "")
+    {
+        return new Status
+        {
+            Version = new ServerVersion
+            {
+                Name = name,
+                Protocol = protocol
+            },
+
+            PlayerInformation = new PlayerInformation
+            {
+                Max = max,
+                Online = online
+            },
+
+            Description = description,
+            Favicon = favicon
+        };
+    }
+}
+
+internal sealed class ServerVersion
+{
+    public required string Name { get; set; }
+
+    public required int Protocol { get; set; }
+}
+
+internal sealed class PlayerInformation
+{
+    public required int Max { get; set; }
+
+    public required int Online { get; set; }
+}
