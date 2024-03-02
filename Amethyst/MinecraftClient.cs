f01a1ce75037f783007af819b14966f01b9d8c70
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-03-02 21:47:10 +0300

    Add client

---
+++ Amethyst/MinecraftClient.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.Extensions.Logging;
+
+namespace Amethyst;
+
+internal sealed class MinecraftClient(
+    ILogger<MinecraftClient> logger,
+    ConnectionContext connection);
