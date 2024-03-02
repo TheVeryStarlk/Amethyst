5ef926dc2d3fe1c210f88fc4ac36619728f257dc
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-03-02 21:58:59 +0300

    Add client state

--- Amethyst/MinecraftClient.cs
+++ Amethyst/MinecraftClient.cs
@@ -5,4 +5,16 @@ namespace Amethyst;
 
 internal sealed class MinecraftClient(
     ILogger<MinecraftClient> logger,
-    ConnectionContext connection);
+    ConnectionContext connection)
+{
+    private MinecraftClientState state;
+}
+
+internal enum MinecraftClientState
+{
+    Handshaking,
+    Status,
+    Login,
+    Playing,
+    Disconnected
+}
