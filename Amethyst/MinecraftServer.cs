55e831fe489552ded26ac96a625ab841b5575b1e
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-03-02 23:11:28 +0300

    Implement connection listening

--- Amethyst/MinecraftServer.cs
+++ Amethyst/MinecraftServer.cs
@@ -1,6 +1,8 @@
-﻿using System.Net;
-using Amethyst.Components;
+﻿using Amethyst.Components;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Amethyst;
 
@@ -8,9 +10,11 @@ internal sealed class MinecraftServer(MinecraftServerConfiguration configuration
 {
     private const int ProtocolVersion = 47;
 
+    private IConnectionListener? listener;
+
     private readonly ILogger<MinecraftServer> logger = configuration.LoggerFactory.CreateLogger<MinecraftServer>();
-    private readonly IPEndPoint listeningEndPoint = configuration.ListeningEndPoint;
     private readonly CancellationTokenSource source = new CancellationTokenSource();
+    private readonly Dictionary<int, MinecraftClient> clients = [];
 
     public Status Status
     {
@@ -18,17 +22,83 @@ internal sealed class MinecraftServer(MinecraftServerConfiguration configuration
         set => throw new NotImplementedException();
     }
 
+    public Task StartAsync()
+    {
+        if (listener is not null)
+        {
+            throw new InvalidOperationException("Server has already started.");
+        }
+
+        logger.LogDebug("Starting the server");
+        return Task.WhenAll(ListeningAsync(), TickingAsync());
+    }
+
     public void Shutdown()
     {
         logger.LogDebug("Shutting down the server");
         source.Cancel();
+    }
+
+    private async Task ListeningAsync()
+    {
+        var factory = new SocketTransportFactory(
+            Options.Create(new SocketTransportOptions()),
+            configuration.LoggerFactory);
+
+        listener = await factory.BindAsync(configuration.ListeningEndPoint);
+        logger.LogInformation("Listening for connection at {Port}", configuration.ListeningEndPoint.Port);
+
+        var identifier = 0;
+
+        while (true)
+        {
+            try
+            {
+                var connection = await listener.AcceptAsync(source.Token);
+
+                if (connection is not null)
+                {
+                    clients[identifier++] = new MinecraftClient(
+                        configuration.LoggerFactory.CreateLogger<MinecraftClient>(),
+                        connection);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+        }
+    }
+
+    private Task TickingAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (listener is not null)
+        {
+            await listener.DisposeAsync();
+        }
+
+        source.Dispose();
+
+        foreach (var client in clients)
+        {
+            await client.Value.DisposeAsync();
+        }
     }
 }
 
 /// <summary>
 /// Represents a Minecraft server.
 /// </summary>
-internal interface IMinecraftServer
+internal interface IMinecraftServer : IAsyncDisposable
 {
     /// <summary>
     /// Stores the server's status, contains player information and MOTD.
