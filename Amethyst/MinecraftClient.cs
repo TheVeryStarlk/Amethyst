279bb06aad73da252e17a79857b28ef1f19989b5
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-03-03 22:43:21 +0300

    Code style thing

--- Amethyst/MinecraftClient.cs
+++ Amethyst/MinecraftClient.cs
@@ -3,10 +3,8 @@ using Microsoft.Extensions.Logging;
 
 namespace Amethyst;
 
-internal sealed class MinecraftClient(
-    ILogger<MinecraftClient> logger,
-    ConnectionContext connection,
-    int identifier) : IAsyncDisposable
+internal sealed class MinecraftClient(ILogger<MinecraftClient> logger, ConnectionContext connection, int identifier)
+    : IAsyncDisposable
 {
     public int Identifier => identifier;
 
