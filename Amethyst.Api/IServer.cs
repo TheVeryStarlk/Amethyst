aef6b6f7ab944668765fe28aa10f0dae8f6706ce
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-04-04 05:55:49 +0300

    We... Start... Small

---
+++ Amethyst.Api/IServer.cs
@@ -0,0 +1,8 @@
+﻿using Amethyst.Api.Entities;
+
+namespace Amethyst.Api;
+
+public interface IServer
+{
+    public IEnumerable<IPlayer> Players { get; }
+}
---
+++ Amethyst/Server.cs
@@ -0,0 +1,9 @@
+﻿using Amethyst.Api;
+using Amethyst.Api.Entities;
+
+namespace Amethyst;
+
+internal sealed class Server : IServer
+{
+    public IEnumerable<IPlayer> Players { get; }
+}
