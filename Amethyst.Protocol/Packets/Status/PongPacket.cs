5245d502845f4a16bd6904943fabcc20da2f0331
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-01-29 00:29:19 +0300

    Add handshake packet's properties and added ping/pong packets.

---
+++ Amethyst.Protocol/Packets/Status/PongPacket.cs
@@ -0,0 +1,15 @@
+﻿namespace Amethyst.Protocol.Packets.Status;
+
+public sealed class PongPacket : IOutgoingPacket
+{
+    public int Identifier => 1;
+
+    int IOutgoingPacket.Length => sizeof(long);
+
+    public required long Magic { get; init; }
+
+    void IOutgoingPacket.Write(ref SpanWriter writer)
+    {
+        writer.WriteLong(Magic);
+    }
+}
