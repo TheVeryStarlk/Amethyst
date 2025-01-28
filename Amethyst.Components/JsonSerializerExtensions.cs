6f2c0b910f291fd4c26c3933110d8b0ad394d882
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-01-29 00:54:03 +0300

    Create a serialization options.

---
+++ Amethyst.Components/JsonSerializerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Amethyst.Components.Messages;
+
+namespace Amethyst.Components;
+
+public static class JsonSerializerExtensions
+{
+    public static JsonSerializerOptions Options { get; } =
+        new()
+        {
+            AllowTrailingCommas = true,
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            NumberHandling = JsonNumberHandling.AllowReadingFromString,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Converters =
+            {
+                new JsonStringEnumConverter<Color>(JsonNamingPolicy.SnakeCaseLower)
+            }
+        };
+}
