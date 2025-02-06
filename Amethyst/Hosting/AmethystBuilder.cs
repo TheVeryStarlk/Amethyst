454ae6488c4c1c665308a479258d6a1f5dcaf4d0
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-02-06 03:54:15 +0300

    Fix small bug.

--- Amethyst/Hosting/AmethystBuilder.cs
+++ Amethyst/Hosting/AmethystBuilder.cs
@@ -9,7 +9,7 @@ internal sealed class AmethystBuilder(IServiceCollection services) : IAmethystBu
 {
     public IAmethystBuilder AddSubscriber<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>() where T : class, ISubscriber
     {
-        services.AddTransient<T>();
+        services.AddSingleton<ISubscriber, T>();
         return this;
     }
 }
