064736fd2380497a216f27506894a59a21f1ece2
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-02-07 04:26:19 +0300

    Write docs for IClient's write method.

--- Amethyst.Abstractions/IClient.cs
+++ Amethyst.Abstractions/IClient.cs
@@ -5,6 +5,13 @@ namespace Amethyst.Abstractions;
 
 public interface IClient
 {
+    /// <summary>
+    /// Writes packet(s) to the client.
+    /// </summary>
+    /// <remarks>
+    /// This is intended to be internally used in Amethyst.
+    /// </remarks>
+    /// <param name="packets">The packet(s) to write.</param>
     public void Write(params ReadOnlySpan<IOutgoingPacket> packets);
 
     public void Stop(Message message);
