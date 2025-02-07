db372e3d21c297b7524f098d4abcd775b59eba66
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-02-07 04:13:33 +0300

    Write docs for create-able and writeable.

--- Amethyst.Protocol/IIngoingPacket.cs
+++ Amethyst.Protocol/IIngoingPacket.cs
@@ -7,8 +7,20 @@ public interface IIngoingPacket
     public static abstract int Identifier { get; }
 }
 
+/// <summary>
+/// Represents a creatable <see cref="IIngoingPacket"/>.
+/// </summary>
+/// <remarks>
+/// This is intended to be internally used in Amethyst.
+/// </remarks>
+/// <typeparam name="T">The packet's type.</typeparam>
 public interface ICreatable<out T> where T : IIngoingPacket
 {
+    /// <summary>
+    /// Creates a new instance of <see cref="T"/> from a <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
+    /// <param name="span">The <see cref="ReadOnlySpan{T}"/> used for creation.</param>
+    /// <returns>A new instance of <see cref="T"/>.</returns>
     public static abstract T Create(ReadOnlySpan<byte> span);
 }
 
--- Amethyst.Protocol/IOutgoingPacket.cs
+++ Amethyst.Protocol/IOutgoingPacket.cs
@@ -5,9 +5,22 @@ public interface IOutgoingPacket
     public int Identifier { get; }
 }
 
+/// <summary>
+/// Represents a writeable <see cref="IOutgoingPacket"/>.
+/// </summary>
+/// <remarks>
+/// This is intended to be internally used in Amethyst.
+/// </remarks>
 public interface IWriteable
 {
+    /// <summary>
+    /// The <see cref="IOutgoingPacket"/>'s length.
+    /// </summary>
     public int Length { get; }
 
+    /// <summary>
+    /// Writes the <see cref="IOutgoingPacket"/> instance to a <see cref="Span{T}"/>.
+    /// </summary>
+    /// <param name="span">The <see cref="Span{T}"/> to write to.</param>
     public void Write(Span<byte> span);
 }
