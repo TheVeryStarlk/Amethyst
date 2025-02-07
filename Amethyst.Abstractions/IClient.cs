217acd495c1f1ddfc4b7af8c1bc471fe93ca3f48
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-02-07 04:23:18 +0300

    Create an IClient.

---
+++ Amethyst.Abstractions/IClient.cs
@@ -0,0 +1,11 @@
+﻿using Amethyst.Abstractions.Messages;
+using Amethyst.Protocol;
+
+namespace Amethyst.Abstractions;
+
+public interface IClient
+{
+    public void Write(params ReadOnlySpan<IOutgoingPacket> packets);
+
+    public void Stop(Message message);
+}
