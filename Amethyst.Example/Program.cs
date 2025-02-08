21c98a830d40749a3338cf9fa8e60bf73d02059f
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-02-08 20:27:35 +0300

    Set minimum logging level to trace.

--- Amethyst.Example/Program.cs
+++ Amethyst.Example/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Amethyst.Example;
 using Amethyst.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = Host.CreateApplicationBuilder();
+
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
 
 builder.Services.AddAmethyst(options => options.AddSubscriber<Subscriber>());
 
