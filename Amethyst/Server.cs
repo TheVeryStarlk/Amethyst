7fb5bfd63fd97c34060c99088302d4a06dccbba1
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-02-08 04:19:13 +0300

    Server.

---
+++ Amethyst/Server.cs
@@ -0,0 +1,13 @@
+﻿using Amethyst.Components;
+
+namespace Amethyst;
+
+internal sealed class Server : IServer
+{
+    private readonly CancellationTokenSource source = new();
+
+    public void Stop()
+    {
+        source.Cancel();
+    }
+}
