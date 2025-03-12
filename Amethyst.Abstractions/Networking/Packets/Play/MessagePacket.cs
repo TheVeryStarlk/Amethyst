bbe80b1f322bf92bf88ece9a73f15c3dee4f4e0c
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-13 02:49:26 +0300

    Implement player class.

---
+++ Amethyst.Abstractions/Networking/Packets/Play/MessagePacket.cs
@@ -0,0 +1,6 @@
+﻿using Amethyst.Abstractions.Entities.Player;
+using Amethyst.Abstractions.Messages;
+
+namespace Amethyst.Abstractions.Networking.Packets.Play;
+
+public sealed record MessagePacket(Message Message, MessagePosition Position) : IOutgoingPacket;
---
+++ Amethyst/Entities/Player.cs
@@ -0,0 +1,38 @@
+﻿using Amethyst.Abstractions;
+using Amethyst.Abstractions.Entities;
+using Amethyst.Abstractions.Entities.Player;
+using Amethyst.Abstractions.Messages;
+using Amethyst.Abstractions.Networking.Packets.Play;
+using Amethyst.Abstractions.Worlds;
+
+namespace Amethyst.Entities;
+
+internal sealed record Player(int Identifier, IClient Client, string Unique, string Username, IWorld World) : IPlayer
+{
+    public Location Location { get; set; }
+
+    public float Yaw { get; set; }
+
+    public float Pitch { get; set; }
+
+    public bool OnGround { get; set; }
+
+    public IWorld World { get; set; } = World;
+
+    public GameMode GameMode { get; set; }
+
+    public string? Locale { get; set; }
+
+    public byte ViewDistance { get; set; }
+
+    public void Send(Message message, MessagePosition position = MessagePosition.Box)
+    {
+        Client.Write(new MessagePacket(message, position));
+    }
+
+    public void Disconnect(Message message)
+    {
+        // We trust the client to actually disconnect.
+        Client.Write(new DisconnectPacket(message));
+    }
+}
