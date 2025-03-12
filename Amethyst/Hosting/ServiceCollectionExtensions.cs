c9bcbcb411b8f513a3dd5a0cff5993c8ec8ba830
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-12 03:03:44 +0300

    Hosting folder.

---
+++ Amethyst/Hosting/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Amethyst.Hosting;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAmethyst(this IServiceCollection services)
+    {
+        return services;
+    }
+}
