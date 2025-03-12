d03418ce84cecf309caaf7744cf2090f33ab888b
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-13 02:53:45 +0300

    Initial implementation for client.

---
+++ Amethyst/Client.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using Amethyst.Abstractions;
+using Amethyst.Abstractions.Entities.Player;
+using Amethyst.Abstractions.Networking;
+using Amethyst.Entities;
+using Amethyst.Eventing;
+using Microsoft.Extensions.Logging;
+
+namespace Amethyst;
+
+internal sealed class Client(ILogger<Client> logger, Socket socket, EventDispatcher eventDispatcher) : IClient, IDisposable
+{
+    // Probably shouldn't use random.
+    public int Identifier { get; } = Random.Shared.Next();
+
+    public IPlayer Player => player ?? throw new InvalidOperationException();
+
+    private Player? player;
+
+    private readonly CancellationTokenSource source = new();
+
+    public void Write(IOutgoingPacket packet)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        source.Cancel();
+    }
+
+    public void Dispose()
+    {
+        source.Dispose();
+    }
+}
