78a4ed719f5d56b434a86401ffda66b94674bbdf
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-15 01:07:22 +0300

    Register status classes for the SG.

--- Amethyst.Abstractions/Messages/AmethystJsonSerializerContext.cs
+++ Amethyst.Abstractions/Messages/AmethystJsonSerializerContext.cs
@@ -3,4 +3,7 @@
 namespace Amethyst.Abstractions.Messages;
 
 [JsonSerializable(typeof(Message))]
+[JsonSerializable(typeof(Status))]
+[JsonSerializable(typeof(Version))]
+[JsonSerializable(typeof(Information))]
 internal sealed partial class AmethystJsonSerializerContext : JsonSerializerContext;
