7e5348e2a651d5f70f9f3a5838de7417eceaacf5
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-15 03:19:00 +0300

    Better status packet.

--- Amethyst.Abstractions/Networking/Packets/Status/StatusResponsePacket.cs
+++ Amethyst.Abstractions/Networking/Packets/Status/StatusResponsePacket.cs
@@ -1,12 +1,44 @@
-﻿namespace Amethyst.Abstractions.Networking.Packets.Status;
+﻿using System.Text.Json.Nodes;
+using Amethyst.Abstractions.Messages;
 
-// Probably should take a status class.
-public sealed class StatusResponsePacket(string status) : IOutgoingPacket
+namespace Amethyst.Abstractions.Networking.Packets.Status;
+
+public sealed class StatusResponsePacket(string name, int numerical, int maximum, int online, Message description, string favicon) : IOutgoingPacket
 {
-    public int Length => Variable.GetByteCount(status);
+    public int Length
+    {
+        get
+        {
+            var version = new JsonObject
+            {
+                ["name"] = name,
+                ["protocol"] = numerical
+            };
+
+            var players = new JsonObject
+            {
+                ["max"] = maximum,
+                ["online"] = online
+            };
+
+            var parent = new JsonObject
+            {
+                ["version"] = version,
+                ["players"] = players,
+                ["description"] = JsonNode.Parse(description.Serialize()),
+                ["favicon"] = favicon
+            };
+
+            serialized = parent.ToJsonString();
+
+            return Variable.GetByteCount(serialized);
+        }
+    }
+
+    private string? serialized;
 
     public void Write(Span<byte> span)
     {
-        SpanWriter.Create(span).WriteVariableString(status);
+        SpanWriter.Create(span).WriteVariableString(serialized!);
     }
 }
