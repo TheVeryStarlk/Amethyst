b7f950893140c4b05067f1bb13954695ed86dc5d
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-17 00:11:39 +0300

    Initial implementation on chunks and sections.

---
+++ Amethyst/Worlds/Chunk.cs
@@ -0,0 +1,44 @@
+﻿using Amethyst.Abstractions.Networking.Packets.Play;
+using Amethyst.Abstractions.Worlds;
+
+namespace Amethyst.Worlds;
+
+internal sealed record Chunk(int X, int Z) : IChunk
+{
+    private readonly Section?[] sections = new Section[16];
+    private readonly Biome[] biomes = new Biome[256];
+
+    public Block this[int x, int y, int z]
+    {
+        get => GetSection(y).GetBlock(x, y, z);
+        set => GetSection(y).SetBlock(value, x, y, z);
+    }
+
+    public Biome this[int x, int z]
+    {
+        get => biomes[NumericUtility.AsIndex(x, z)];
+        set => biomes[NumericUtility.AsIndex(x, z)] = value;
+    }
+
+    public SingleChunkPacket Build()
+    {
+        var parts = sections.OfType<Section>().Select(section => section.Build()).ToArray();
+
+        var final = Enumerable.Repeat<byte>(255, 12288 * parts.Length + 256).ToArray();
+        var index = 0;
+
+        foreach (var part in parts)
+        {
+            part.CopyTo(final, index);
+            index += 8192;
+        }
+
+        return new SingleChunkPacket(X, Z, final, (ushort) ((1 << parts.Length) - 1));
+    }
+
+    private Section GetSection(int y)
+    {
+        var index = y >> 4;
+        return sections[index] ?? (sections[index] = new Section());
+    }
+}
---
+++ Amethyst/Worlds/Section.cs
@@ -0,0 +1,33 @@
+﻿using Amethyst.Abstractions.Worlds;
+
+namespace Amethyst.Worlds;
+
+internal sealed class Section
+{
+    private readonly byte[] blocks = new byte[8192];
+
+    public Block GetBlock(int x, int y, int z)
+    {
+        var index = NumericUtility.AsIndex(x, y, z) * 2;
+
+        var type = blocks[index] >> 4 | blocks[index + 1] << 4;
+        var metadata = blocks[index] & 0x0F;
+
+        return new Block(type, metadata);
+    }
+
+    public void SetBlock(Block block, int x, int y, int z)
+    {
+        var index = NumericUtility.AsIndex(x, y, z) * 2;
+        var type = block.Type;
+
+        blocks[index] = (byte) (type << 4 | block.Metadata);
+        blocks[index + 1] = (byte) (type >> 4);
+    }
+
+    public byte[] Build()
+    {
+        // Lighting is not implemented "yet".
+        return blocks;
+    }
+}
