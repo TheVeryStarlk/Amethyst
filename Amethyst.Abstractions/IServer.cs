f1a1d88a4947d56e562137c6b9e2fe147707ceff
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-16 06:15:13 +0300

    Comment.

--- Amethyst.Abstractions/IServer.cs
+++ Amethyst.Abstractions/IServer.cs
@@ -4,6 +4,7 @@ namespace Amethyst.Abstractions;
 
 public interface IServer
 {
+    // Probably should just be an injectable class?
     public IWorldCollection Worlds { get; }
 
     public void Stop();
