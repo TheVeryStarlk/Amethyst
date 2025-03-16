b75d0481a98f3d8616b3b0f2bd33ad5cf6c5a7d0
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-16 06:03:06 +0300

    Add config packet.

---
+++ Amethyst/Networking/Packets/Play/ConfigurationPacket.cs
@@ -0,0 +1,18 @@
+﻿namespace Amethyst.Networking.Packets.Play;
+
+internal sealed record ConfigurationPacket(string Locale, byte ViewDistance) : IIngoingPacket<ConfigurationPacket>, IProcessor
+{
+    public static int Identifier => 21;
+
+    public static ConfigurationPacket Create(ReadOnlySpan<byte> span)
+    {
+        var reader = new SpanReader(span);
+        return new ConfigurationPacket(reader.ReadVariableString(), reader.ReadByte());
+    }
+
+    public void Process(Client client)
+    {
+        client.Player!.Locale = Locale;
+        client.Player.ViewDistance = ViewDistance;
+    }
+}
