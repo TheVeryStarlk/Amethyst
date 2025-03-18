c4ccb76cb36c7f99cf2abaa1c20c26903d753c17
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-18 05:58:20 +0300

    Remove docs.

--- Amethyst.Abstractions/IClient.cs
+++ Amethyst.Abstractions/IClient.cs
@@ -1,32 +1,12 @@
-﻿using Amethyst.Abstractions.Entities.Player;
-using Amethyst.Abstractions.Networking.Packets;
+﻿using Amethyst.Abstractions.Networking.Packets;
 
 namespace Amethyst.Abstractions;
 
-/// <summary>
-/// Represents a Minecraft connection.
-/// </summary>
 public interface IClient
 {
-    /// <summary>
-    /// A unique number shared by <see cref="IClient"/> and <see cref="IPlayer"/>
-    /// </summary>
     public int Identifier { get; }
 
-    /// <summary>
-    /// Writes an <see cref="IOutgoingPacket"/> to the <see cref="IClient"/>.
-    /// </summary>
-    /// <remarks>
-    /// Misusing this method might lead to weird behaviors and/or bugs.
-    /// </remarks>
-    /// <param name="packet">The <see cref="IOutgoingPacket"/> to write.</param>
     public void Write(IOutgoingPacket packet);
 
-    /// <summary>
-    /// Stops the <see cref="IClient"/>'s connection.
-    /// </summary>
-    /// <remarks>
-    /// Use <see cref="IPlayer.Disconnect"/> for a graceful disconnection.
-    /// </remarks>
     public void Stop();
 }
