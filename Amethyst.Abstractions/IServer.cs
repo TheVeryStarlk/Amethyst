56051b78c6f1b3512507870ff54a520c221e5864
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-18 21:18:13 +0300

    No need to expose a collection of worlds.

--- Amethyst.Abstractions/IServer.cs
+++ Amethyst.Abstractions/IServer.cs
@@ -4,9 +4,7 @@ namespace Amethyst.Abstractions;
 
 public interface IServer
 {
-    public IReadOnlyDictionary<string, IWorld> Worlds { get; }
-
-    public void Create(string name, WorldType type, Dimension dimension, Difficulty difficulty, IGenerator generator);
+    public IWorld Create(string name, WorldType type, Dimension dimension, Difficulty difficulty, IGenerator generator);
 
     public void Stop();
 }
