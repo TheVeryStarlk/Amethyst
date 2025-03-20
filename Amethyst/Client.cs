363411a0e56ad0ee955d3f9d6579fe62a90f39c0
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-20 04:23:34 +0300

    Write a scratch for client.

--- Amethyst/Client.cs
+++ Amethyst/Client.cs
@@ -20,6 +20,28 @@ internal sealed class Client(ILogger<Client> logger, Socket socket, EventDispatc
 
     private State state;
 
+    public async Task StartAsync()
+    {
+        var reading = ReadingAsync();
+        var writing = WritingAsync();
+
+        // If reading finishes first, then either the client stopped the connection or the token was cancelled,
+        // but there still is some work to do in the writing task, so wait for that work to finish.
+        if (await Task.WhenAny(reading, writing).ConfigureAwait(false) == reading)
+        {
+            if (!source.IsCancellationRequested)
+            {
+                outgoing.Writer.Complete();
+            }
+
+            await writing.ConfigureAwait(false);
+        }
+
+        // Writing might finish before reading, so in that case cancelling the token will stop
+        // the reading task thus marking the end of the entire reading writing process.
+        source.Cancel();
+    }
+
     public void Write(params ReadOnlySpan<IOutgoingPacket> packets)
     {
         if (source.IsCancellationRequested)
@@ -45,6 +67,43 @@ internal sealed class Client(ILogger<Client> logger, Socket socket, EventDispatc
     public void Dispose()
     {
         source.Dispose();
+        socket.Dispose();
+    }
+
+    private async Task ReadingAsync()
+    {
+        while (true)
+        {
+            try
+            {
+                await socket.ReceiveAsync(ArraySegment<byte>.Empty, source.Token).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+            catch (Exception exception)
+            {
+                logger.LogError(exception, "Exception while reading");
+                break;
+            }
+        }
+    }
+
+    private async Task WritingAsync()
+    {
+        await foreach (var packet in outgoing.Reader.ReadAllAsync().ConfigureAwait(false))
+        {
+            try
+            {
+                // Work...
+            }
+            catch (Exception exception)
+            {
+                logger.LogError(exception, "Exception while writing");
+                break;
+            }
+        }
     }
 }
 
