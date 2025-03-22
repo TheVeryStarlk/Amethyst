5d37b5eb10209c096cb45284183fb6a23f18afae
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-03-22 07:44:14 +0300

    Start implementing server.

---
+++ Amethyst/BalancingTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace Amethyst;
+
+internal sealed class BalancingTimer(TimeSpan timeSpan)
+{
+    private readonly long ticks = timeSpan.Milliseconds * Stopwatch.Frequency / 1000L;
+    private readonly Stopwatch stopwatch = new();
+
+    private long delay;
+
+    public async ValueTask<bool> WaitForNextTickAsync(CancellationToken cancellationToken)
+    {
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return false;
+        }
+
+        var delta = stopwatch.ElapsedTicks;
+        stopwatch.Restart();
+
+        delay += delta - ticks;
+
+        if (delay >= 0)
+        {
+            return true;
+        }
+
+        var extraTimeInMilliseconds = (int) (-delay * 1000L / Stopwatch.Frequency);
+        await Task.Delay(extraTimeInMilliseconds, cancellationToken).ConfigureAwait(false);
+
+        return true;
+    }
+}
---
+++ Amethyst/Server.cs
@@ -0,0 +1,23 @@
+﻿using Amethyst.Abstractions;
+using Amethyst.Abstractions.Worlds;
+using Amethyst.Eventing;
+using Amethyst.Worlds;
+using Microsoft.Extensions.Logging;
+
+namespace Amethyst;
+
+internal sealed class Server(ILoggerFactory loggerFactory, EventDispatcher eventDispatcher) : IServer
+{
+    private readonly ILogger<Server> logger = loggerFactory.CreateLogger<Server>();
+    private readonly CancellationTokenSource source = new();
+
+    public IWorld Create(string name, WorldType type, Dimension dimension, Difficulty difficulty, IGenerator generator)
+    {
+        return new World(name, type, dimension, difficulty, generator);
+    }
+
+    public void Stop()
+    {
+        source.Cancel();
+    }
+}
