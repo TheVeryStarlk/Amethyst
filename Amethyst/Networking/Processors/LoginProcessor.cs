538f9209bad053d1f8a7bcce69034b987158348f
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-05 01:19:11 +0300

    Throw if no world was set!

--- Amethyst/Networking/Processors/LoginProcessor.cs
+++ Amethyst/Networking/Processors/LoginProcessor.cs
@@ -13,20 +13,17 @@ internal sealed class LoginProcessor : IProcessor
 {
     public static void Process(Client client, Packet packet)
     {
-        var start = packet.Create<StartPacket>();
-        var login = client.EventDispatcher.Dispatch(client, new Login(start.Username));
+        var login = client.EventDispatcher.Dispatch(client, new Login(packet.Create<StartPacket>().Username));
+        var world = login.World ?? throw new InvalidOperationException("No world was set.");
 
-        if (login.World is { } world)
-        {
-            client.Player = new Player(client, Guid.NewGuid().ToString(), start.Username, world);
+        client.Player = new Player(client, Guid.NewGuid().ToString(), login.Username, world);
 
-            client.Write(
-                new SuccessPacket(client.Player.Unique, client.Player.Username),
-                new JoinGamePacket(client.Player.Identifier, login.GameMode, world.Dimension, world.Difficulty, byte.MaxValue, world.Type, false),
-                new PositionLookPacket(new Location(), 0, 0));
+        client.Write(
+            new SuccessPacket(client.Player.Unique, client.Player.Username),
+            new JoinGamePacket(client.Player.Identifier, login.GameMode, world.Dimension, world.Difficulty, byte.MaxValue, world.Type, false),
+            new PositionLookPacket(new Location(), 0, 0));
 
-            client.State = State.Play;
-            client.EventDispatcher.Dispatch(client.Player, new Joined());
-        }
+        client.State = State.Play;
+        client.EventDispatcher.Dispatch(client.Player, new Joined());
     }
 }
