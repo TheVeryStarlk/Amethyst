48f8c921d9c07e78f6cf2fb19bd33db0d269ccf2
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-07 04:03:01 +0300

    Implement state packet.

---
+++ Amethyst.Abstractions/Networking/Packets/Play/ChangeStatePacket.cs
@@ -0,0 +1,57 @@
+﻿using Amethyst.Abstractions.Entities.Player;
+
+namespace Amethyst.Abstractions.Networking.Packets.Play;
+
+public sealed class ChangeStatePacket(IState state) : IOutgoingPacket
+{
+    public int Identifier => 43;
+
+    public IState State => state;
+}
+
+public interface IState
+{
+    public byte Identifier { get; }
+}
+
+public interface IValueState : IState
+{
+    public float Value { get; }
+}
+
+public sealed class StartRainingState : IState
+{
+    public byte Identifier => 1;
+}
+
+public sealed class StopRainingState : IState
+{
+    public byte Identifier => 2;
+}
+
+public sealed class GameModeState(GameMode gameMode) : IValueState
+{
+    public byte Identifier => 3;
+
+    public float Value => (byte) gameMode;
+}
+
+public sealed class EnterCreditsState : IState
+{
+    public byte Identifier => 4;
+}
+
+public sealed class DemoState(Demo demo) : IValueState
+{
+    public byte Identifier => 5;
+
+    public float Value => (byte) demo;
+}
+
+public enum Demo
+{
+    Welcome,
+    MovementControls = 101,
+    JumpControls = 102,
+    InventoryControls = 103
+}
---
+++ Amethyst/Networking/Serializers/Play/ChangeStateSerializer.cs
@@ -0,0 +1,24 @@
+﻿using Amethyst.Abstractions.Networking;
+using Amethyst.Abstractions.Networking.Packets.Play;
+
+namespace Amethyst.Networking.Serializers.Play;
+
+internal sealed class ChangeStateSerializer(IState state) : ISerializer<ChangeStatePacket, ChangeStateSerializer>
+{
+    public int Length => sizeof(byte) + sizeof(float);
+
+    public static ChangeStateSerializer Create(ChangeStatePacket packet)
+    {
+        return new ChangeStateSerializer(packet.State);
+    }
+
+    public void Write(Span<byte> span)
+    {
+        var writer = SpanWriter.Create(span).WriteByte(state.Identifier);
+
+        if (state is IValueState value)
+        {
+            writer.WriteFloat(value.Value);
+        }
+    }
+}
