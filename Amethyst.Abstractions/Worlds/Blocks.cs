44b86ff3f540cae224e090cab196b101169e2959
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-15 03:12:41 +0300

    Add more blocks.

--- Amethyst.Abstractions/Worlds/Blocks.cs
+++ Amethyst.Abstractions/Worlds/Blocks.cs
@@ -1,6 +1,365 @@
 ﻿namespace Amethyst.Abstractions.Worlds;
 
+// Some blocks are missing.
 public static class Blocks
 {
-    public static Block Air { get; } = new(0);
+    public static Block Air => new(0);
+
+    public static Block Stone => new(1);
+
+    public static Block GrassBlock => new(2);
+
+    public static Block Dirt => new(3);
+
+    public static Block Cobblestone => new(4);
+
+    public static Block WoodPlanks => new(5);
+
+    public static Block Sapling => new(6);
+
+    public static Block Bedrock => new(7);
+
+    public static Block Water => new(8);
+
+    public static Block StationaryWater => new(9);
+
+    public static Block Lava => new(10);
+
+    public static Block StationaryLava => new(11);
+
+    public static Block Sand => new(12);
+
+    public static Block Gravel => new(13);
+
+    public static Block GoldOre => new(14);
+
+    public static Block IronOre => new(15);
+
+    public static Block CoalOre => new(16);
+
+    public static Block Wood => new(17);
+
+    public static Block Leaves => new(18);
+
+    public static Block Sponge => new(19);
+
+    public static Block Glass => new(20);
+
+    public static Block LapisLazuliOre => new(21);
+
+    public static Block LapisLazuliBlock => new(22);
+
+    public static Block Dispenser => new(23);
+
+    public static Block Sandstone => new(24);
+
+    public static Block NoteBlock => new(25);
+
+    public static Block Bed => new(26);
+
+    public static Block PoweredRail => new(27);
+
+    public static Block DetectorRail => new(28);
+
+    public static Block StickyPiston => new(29);
+
+    public static Block Cobweb => new(30);
+
+    public static Block Grass => new(31);
+
+    public static Block DeadBush => new(32);
+
+    public static Block Piston => new(33);
+
+    public static Block PistonHead => new(34);
+
+    public static Block Wool => new(35);
+
+    public static Block Dandelion => new(37);
+
+    public static Block Poppy => new(38);
+
+    public static Block Gold => new(41);
+
+    public static Block Iron => new(42);
+
+    public static Block DoubleStoneSlab => new(43);
+
+    public static Block StoneSlab => new(44);
+
+    public static Block Bricks => new(45);
+
+    public static Block Explosive => new(46);
+
+    public static Block Bookshelf => new(47);
+
+    public static Block MossStone => new(48);
+
+    public static Block Obsidian => new(49);
+
+    public static Block Torch => new(50);
+
+    public static Block Fire => new(51);
+
+    public static Block MonsterSpawner => new(52);
+
+    public static Block OakWoodStairs => new(53);
+
+    public static Block Chest => new(54);
+
+    public static Block RedstoneWire => new(55);
+
+    public static Block DiamondOre => new(56);
+
+    public static Block Diamond => new(57);
+
+    public static Block CraftingTable => new(58);
+
+    public static Block Wheat => new(59);
+
+    public static Block Farmland => new(60);
+
+    public static Block Furnace => new(61);
+
+    public static Block BurningFurnace => new(62);
+
+    public static Block StandingSign => new(63);
+
+    public static Block OakDoor => new(64);
+
+    public static Block Ladder => new(65);
+
+    public static Block Rail => new(66);
+
+    public static Block CobblestoneStairs => new(67);
+
+    public static Block WallSign => new(68);
+
+    public static Block Lever => new(69);
+
+    public static Block StonePressurePlate => new(70);
+
+    public static Block IronDoor => new(71);
+
+    public static Block WoodenPressurePlate => new(72);
+
+    public static Block RedstoneOre => new(73);
+
+    public static Block GlowingRedstoneOre => new(74);
+
+    public static Block StoneButton => new(77);
+
+    public static Block Ice => new(79);
+
+    public static Block Snow => new(80);
+
+    public static Block Cactus => new(81);
+
+    public static Block Clay => new(82);
+
+    public static Block SugarCane => new(83);
+
+    public static Block Jukebox => new(84);
+
+    public static Block Fence => new(85);
+
+    public static Block Pumpkin => new(86);
+
+    public static Block Netherrack => new(87);
+
+    public static Block SoulSand => new(88);
+
+    public static Block Glowstone => new(89);
+
+    public static Block NetherPortal => new(90);
+
+    public static Block JackoLantern => new(91);
+
+    public static Block Cake => new(92);
+
+    public static Block StainedGlass => new(95);
+
+    public static Block Trapdoor => new(96);
+
+    public static Block MonsterEgg => new(97);
+
+    public static Block StoneBricks => new(98);
+
+    public static Block IronBars => new(101);
+
+    public static Block GlassPane => new(102);
+
+    public static Block Melon => new(103);
+
+    public static Block PumpkinStem => new(104);
+
+    public static Block MelonStem => new(105);
+
+    public static Block Vines => new(106);
+
+    public static Block FenceGate => new(107);
+
+    public static Block BrickStairs => new(108);
+
+    public static Block StoneBrickStairs => new(109);
+
+    public static Block Mycelium => new(110);
+
+    public static Block LilyPad => new(111);
+
+    public static Block NetherBrick => new(112);
+
+    public static Block NetherBrickFence => new(113);
+
+    public static Block NetherBrickStairs => new(114);
+
+    public static Block NetherWart => new(115);
+
+    public static Block EnchantmentTable => new(116);
+
+    public static Block BrewingStand => new(117);
+
+    public static Block Cauldron => new(118);
+
+    public static Block EndPortal => new(119);
+
+    public static Block EndPortalBlock => new(120);
+
+    public static Block EndStone => new(121);
+
+    public static Block DragonEgg => new(122);
+
+    public static Block DoubleWoodenSlab => new(125);
+
+    public static Block WoodenSlab => new(126);
+
+    public static Block Cocoa => new(127);
+
+    public static Block SandstoneStairs => new(128);
+
+    public static Block EmeraldOre => new(129);
+
+    public static Block EnderChest => new(130);
+
+    public static Block TripwireHook => new(131);
+
+    public static Block Tripwire => new(132);
+
+    public static Block Emerald => new(133);
+
+    public static Block SpruceWoodStairs => new(134);
+
+    public static Block BirchWoodStairs => new(135);
+
+    public static Block JungleWoodStairs => new(136);
+
+    public static Block CommandBlock => new(137);
+
+    public static Block Beacon => new(138);
+
+    public static Block CobblestoneWall => new(139);
+
+    public static Block FlowerPot => new(140);
+
+    public static Block Carrot => new(141);
+
+    public static Block Potato => new(142);
+
+    public static Block WoodenButton => new(143);
+
+    public static Block MobHead => new(144);
+
+    public static Block Anvil => new(145);
+
+    public static Block TrappedChest => new(146);
+
+    public static Block DaylightSensor => new(151);
+
+    public static Block Redstone => new(152);
+
+    public static Block NetherQuartzOre => new(153);
+
+    public static Block Hopper => new(154);
+
+    public static Block Quartz => new(155);
+
+    public static Block QuartzStairs => new(156);
+
+    public static Block ActivatorRail => new(157);
+
+    public static Block Dropper => new(158);
+
+    public static Block StainedClay => new(159);
+
+    public static Block StainedGlassPane => new(160);
+
+    public static Block AcaciaWoodStairs => new(163);
+
+    public static Block DarkOakWoodStairs => new(164);
+
+    public static Block SlimeBlock => new(165);
+
+    public static Block Barrier => new(166);
+
+    public static Block IronTrapdoor => new(167);
+
+    public static Block Prismarine => new(168);
+
+    public static Block SeaLantern => new(169);
+
+    public static Block HayBale => new(170);
+
+    public static Block Carpet => new(171);
+
+    public static Block HardenedClay => new(172);
+
+    public static Block Coal => new(173);
+
+    public static Block PackedIce => new(174);
+
+    public static Block LargeFlowers => new(175);
+
+    public static Block StandingBanner => new(176);
+
+    public static Block WallBanner => new(177);
+
+    public static Block InvertedDaylightSensor => new(178);
+
+    public static Block RedSandstone => new(179);
+
+    public static Block RedSandstoneStairs => new(180);
+
+    public static Block DoubleRedSandstoneSlab => new(181);
+
+    public static Block RedSandstoneSlab => new(182);
+
+    public static Block SpruceFenceGate => new(183);
+
+    public static Block BirchFenceGate => new(184);
+
+    public static Block JungleFenceGate => new(185);
+
+    public static Block DarkOakFenceGate => new(186);
+
+    public static Block AcaciaFenceGate => new(187);
+
+    public static Block SpruceFence => new(188);
+
+    public static Block BirchFence => new(189);
+
+    public static Block JungleFence => new(190);
+
+    public static Block DarkOakFence => new(191);
+
+    public static Block AcaciaFence => new(192);
+
+    public static Block SpruceDoor => new(193);
+
+    public static Block BirchDoor => new(194);
+
+    public static Block JungleDoor => new(195);
+
+    public static Block AcaciaDoor => new(196);
+
+    public static Block DarkOakDoor => new(197);
 }
