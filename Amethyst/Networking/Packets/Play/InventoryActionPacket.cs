bb7e7299fe088d6e583dfdd724d750d2f610a4ab
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-21 02:16:36 +0300

    Create an inventory action packet.

---
+++ Amethyst/Networking/Packets/Play/InventoryActionPacket.cs
@@ -0,0 +1,19 @@
+﻿using Amethyst.Abstractions.Entities;
+using Amethyst.Eventing;
+
+namespace Amethyst.Networking.Packets.Play;
+
+internal sealed class InventoryActionPacket(short index, Item item) : IIngoingPacket<InventoryActionPacket>, IProcessor
+{
+    public static int Identifier => 16;
+
+    public static InventoryActionPacket Create(ReadOnlySpan<byte> span)
+    {
+        var reader = new SpanReader(span);
+        return new InventoryActionPacket(reader.ReadShort(), reader.ReadItem());
+    }
+
+    public void Process(Client client, EventDispatcher eventDispatcher)
+    {
+    }
+}
