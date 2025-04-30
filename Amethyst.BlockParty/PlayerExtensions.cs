b4e56e41f41f085c126b8f32004193fb917111fa
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-30 23:29:37 +0300

    Start working on the gameplay.

---
+++ Amethyst.BlockParty/PlayerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Amethyst.Abstractions.Entities.Player;
+using Amethyst.Abstractions.Messages;
+using Amethyst.Abstractions.Packets.Play;
+
+namespace Amethyst.BlockParty;
+
+internal static class PlayerExtensions
+{
+    public static void Send(this IPlayer player, Message message, MessagePosition position = MessagePosition.Box)
+    {
+        player.Client.Write(new MessagePacket(message, position));
+    }
+}
